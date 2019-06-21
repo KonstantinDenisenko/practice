ed91cfddef53b0f1517975b39fc93fa169f91470
Kostiantyn Denysenko <Kostiantyn.Denysenko@teaminternational.com>
2019-06-21 18:36:22 +0300

    oop

---
+++ Task1/js/class.js
@@ -0,0 +1,145 @@
+function Form() {
+    this.date = document.getElementById('inputDate');
+    this.month = document.getElementById('inputMonth');
+    this.year = document.getElementById('inputYear');
+    this.person = document.getElementById('typePerson');
+    this.group = document.getElementById('typeGroup');
+    this.btnPlus = document.getElementById('btnPlus');
+    this.btnMinus = document.getElementById('btnMinus');
+    this.inputCounter = document.getElementById('inputCounter');
+    this.totalPrice = document.getElementById('outputTotalPrice');
+
+    this.months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+    this.saturday = 5;
+    this.sunday = 6;
+    this.bulkQuantity = "10";
+    this.minimumYear = 2009;
+    this.currency = " грн.";
+    this.monthsStr = "";
+    this.finalCost = "";
+    this.fixedFinalCost = "";
+    this.individualPrice = {
+        weeksDay: 50,
+        weekend: 75
+    };
+    this.groupPrice = {
+        weeksDay: 45,
+        weekend: 70
+    };
+
+    this.dateOfYear = new Date();
+    this.currentDate = this.dateOfYear.getFullYear();
+
+    for(this.currentDate; this.currentDate > this.minimumYear; this.currentDate--) {
+        this.option = document.createElement('option');
+        this.option.innerHTML = this.currentDate;
+        this.year.appendChild(this.option);
+    };
+
+    for(let a = 0; a < this.months.length; a++) {
+        this.monthsStr += `<option value='${a}'>${this.months[a]}</option>`;
+    };
+    this.month.innerHTML = this.monthsStr;
+
+    this.createDays = () => {
+        this.date.innerHTML = "";
+        this.numDaysOfMonth = new Date(this.year.value, +this.month.value + 1, 0);
+
+        for(let a = 1; a < this.numDaysOfMonth.getDate() + 1; a++) {
+            this.option = document.createElement('option');
+            this.option.innerHTML = a;
+            this.date.appendChild(this.option);
+        };
+    };
+
+
+    this.events = () => {
+        this.btnPlus.addEventListener('click', this.additionPrice);
+        this.btnMinus.addEventListener('click', this.subtractionPrice);
+        this.date.addEventListener('change', this.checkDayOfWeek);
+        this.month.addEventListener('change', () => {
+            this.checkDayOfWeek();
+            this.createDays();
+        });
+        this.year.addEventListener('change', () => {
+            this.checkDayOfWeek();
+            this.createDays();
+        });
+        this.person.addEventListener('click', this.checkDayOfWeek);
+        this.group.addEventListener('click', this.checkDayOfWeek);
+    };
+
+
+    this.checkDayOfWeek = () => {
+        this.inputCounter.value = 1;
+        this.numDaysOfMonth = new Date(this.year.value, this.month.value, this.date.value);
+        this.utsDayOfWeek = this.numDaysOfMonth.getUTCDay();
+
+        if(this.utsDayOfWeek === this.saturday || this.utsDayOfWeek === this.sunday) {
+            this.checkGroupOrIndividualPrice(this.individualPrice.weekend, this.groupPrice.weekend);
+        } else {
+            this.checkGroupOrIndividualPrice(this.individualPrice.weeksDay, this.groupPrice.weeksDay);
+        }
+    };
+
+
+    this.checkGroupOrIndividualPrice = (individualPrice, groupPrice) => {
+        if(this.person.checked){
+            this.finalCost = individualPrice;
+            this.fixedFinalCost = individualPrice;
+            this.setFinalCost(this.finalCost);
+        } else if(this.group.checked) {
+            this.finalCost = groupPrice;
+            this.fixedFinalCost = groupPrice;
+            this.setFinalCost(this.finalCost);
+        }
+    };
+
+    this.additionPrice = () => {
+        ++this.inputCounter.value;
+        this.finalCost += this.fixedFinalCost;
+        this.chekBulkQuantity();
+    };
+
+    this.chekBulkQuantity = () => {
+        if(this.inputCounter.value === this.bulkQuantity) {
+            this.choosesGroupPrice();
+        }else {
+
+            this.setFinalCost(this.finalCost);
+        }
+    };
+
+    this.choosesGroupPrice = () => {
+        if(this.fixedFinalCost === this.individualPrice.weeksDay) {
+            this.group.checked = true;
+            this.fixedFinalCost = this.groupPrice.weeksDay;
+            this.finalCost = this.groupPrice.weeksDay * this.bulkQuantity;
+            this.setFinalCost(this.finalCost);
+        }else if(this.fixedFinalCost === this.individualPrice.weekend) {
+            this.group.checked = true;
+            this.fixedFinalCost = this.groupPrice.weekend;
+            this.finalCost = this.groupPrice.weekend * this.bulkQuantity;
+            this.setFinalCost(this.finalCost);
+        }else if(this.fixedFinalCost === this.groupPrice.weeksDay || this.fixedFinalCost === this.groupPrice.weekend) {
+            this.setFinalCost(this.finalCost);
+        }
+    };
+
+    this.subtractionPrice = () => {
+        if(this.inputCounter.value > 1) {
+            this.inputCounter.value--;
+            this.finalCost -= this.fixedFinalCost;
+        }
+        this.setFinalCost(this.finalCost);
+    };
+
+    this.setFinalCost = (price) => {
+        this.totalPrice.innerHTML = price + this.currency;
+    };
+};
+
+let form = new Form();
+form.createDays();
+form.events();
+form.checkDayOfWeek();
--- Task1/js/index.js
+++ Task1/js/index.js
@@ -1,18 +1,20 @@
-let $Date = document.getElementById('inputDate');
-let $Month = document.getElementById('inputMonth');
-let $Year = document.getElementById('inputYear');
-let $Person = document.getElementById('typePerson');
-let $Group = document.getElementById('typeGroup');
-let $btnPlus = document.getElementById('btnPlus');
-let $btnMinus = document.getElementById('btnMinus');
-let $inputCounter = document.getElementById('inputCounter');
-let $TotalPrice = document.getElementById('outputTotalPrice');
+const date = document.getElementById('inputDate');
+const month = document.getElementById('inputMonth');
+const year = document.getElementById('inputYear');
+const person = document.getElementById('typePerson');
+const group = document.getElementById('typeGroup');
+const btnPlus = document.getElementById('btnPlus');
+const btnMinus = document.getElementById('btnMinus');
+const inputCounter = document.getElementById('inputCounter');
+const totalPrice = document.getElementById('outputTotalPrice');
 
-const month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
 const saturday = 5;
 const sunday = 6;
+const bulkQuantity = "10";
 const minimumYear = 2009;
-let currency = " грн.";
+const currency = " грн.";
+let monthsStr = "";
 let finalCost;
 let fixedFinalCost;
 let individualPrice = {
@@ -24,62 +26,52 @@ let groupPrice = {
     weekend: 70
 };
 
-let date = new Date();
-let currentDate = date.getFullYear();
+let dateOfYear = new Date();
+let currentDate = dateOfYear.getFullYear();
 
 for(currentDate; currentDate > minimumYear; currentDate--) {
     let option = document.createElement('option');
     option.innerHTML = currentDate;
-    $Year.appendChild(option);
+    year.appendChild(option);
 };
 
-month.forEach( (value, index) => {
-    let option = document.createElement('option');
-    option.innerHTML = value;
-    option.value = index;
-    $Month.appendChild(option);
-});
-
-createDays();
+for(let a = 0; a < months.length; a++) {
+    monthsStr += `<option value='${a}'>${months[a]}</option>`;
+};
+month.innerHTML = monthsStr;
 
 function createDays() {
-    $Date.innerHTML = "";
-    let numDaysOfMonth = new Date($Year.value, +$Month.value + 1, 0);
+    date.innerHTML = "";
+    let numDaysOfMonth = new Date(year.value, +month.value + 1, 0);
 
     for(let a = 1; a < numDaysOfMonth.getDate() + 1; a++) {
         let option = document.createElement('option');
         option.innerHTML = a;
-        $Date.appendChild(option);
+        date.appendChild(option);
     };
 };
+createDays();
 
-$Date.onchange =  () => {
-    checkDayOfWeek();
+function events() {
+    btnPlus.addEventListener('click', additionPrice);
+    btnMinus.addEventListener('click', subtractionPrice);
+    date.addEventListener('change', checkDayOfWeek);
+    month.addEventListener('change', () => {
+        checkDayOfWeek();
+        createDays();
+    });
+    year.addEventListener('change', () => {
+        checkDayOfWeek();
+        createDays();
+    });
+    person.addEventListener('click', checkDayOfWeek);
+    group.addEventListener('click', checkDayOfWeek);
 };
-
-$Month.onchange =  () => {
-    checkDayOfWeek();
-    createDays();
-};
-
-$Year.onchange =  () => {
-    checkDayOfWeek();
-    createDays();
-};
-
-$Person.addEventListener('click', () => {
-    checkDayOfWeek();
-});
-
-$Group.addEventListener('click', () => {
-    checkDayOfWeek();
-});
-
-checkDayOfWeek();
+events();
 
 function checkDayOfWeek() {
-    $inputCounter.value = 1;
-    let numDaysOfMonth = new Date($Year.value, $Month.value, $Date.value);
+    inputCounter.value = 1;
+    let numDaysOfMonth = new Date(year.value, month.value, date.value);
     let utsDayOfWeek = numDaysOfMonth.getUTCDay();
 
     if(utsDayOfWeek === saturday || utsDayOfWeek === sunday) {
@@ -88,33 +80,59 @@ function checkDayOfWeek() {
         checkGroupOrIndividualPrice(individualPrice.weeksDay, groupPrice.weeksDay);
     }
 };
+checkDayOfWeek();
 
 function checkGroupOrIndividualPrice(individualPrice, groupPrice) {
-    if($Person.checked){
+    if(person.checked){
         finalCost = individualPrice;
         fixedFinalCost = individualPrice;
         setFinalCost(individualPrice);
-    } else if($Group.checked) {
+    } else if(group.checked) {
         finalCost = groupPrice;
         fixedFinalCost = groupPrice;
         setFinalCost(groupPrice);
     }
 };
 
-$btnPlus.addEventListener('click', () => {
-    ++$inputCounter.value;
+function additionPrice () {
+    ++inputCounter.value;
     finalCost += fixedFinalCost;
-    setFinalCost(finalCost);
-});
+    chekBulkQuantity();
+};
 
-$btnMinus.addEventListener('click', () => {
-    if($inputCounter.value > 1) {
-        $inputCounter.value--;
+function chekBulkQuantity() {
+    if(inputCounter.value === bulkQuantity) {
+        choosesGroupPrice();
+    }else {
+
+        setFinalCost(finalCost);
+    }
+};
+
+function choosesGroupPrice() {
+    if(fixedFinalCost === individualPrice.weeksDay) {
+        group.checked = true;
+        fixedFinalCost = groupPrice.weeksDay;
+        finalCost = groupPrice.weeksDay * bulkQuantity;
+        setFinalCost(finalCost);
+    }else if(fixedFinalCost === individualPrice.weekend) {
+        group.checked = true;
+        fixedFinalCost = groupPrice.weekend;
+        finalCost = groupPrice.weekend * bulkQuantity;
+        setFinalCost(finalCost);
+    }else if(fixedFinalCost === groupPrice.weeksDay || fixedFinalCost === groupPrice.weekend) {
+        setFinalCost(finalCost);
+    }
+};
+
+function subtractionPrice () {
+    if(inputCounter.value > 1) {
+        inputCounter.value--;
         finalCost -= fixedFinalCost;
     }
     setFinalCost(finalCost);
-});
+};
 
 function setFinalCost(price) {
-    $TotalPrice.innerHTML = price + currency;
+    totalPrice.innerHTML = price + currency;
 };
